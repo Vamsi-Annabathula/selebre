ed0a4e32b065dd24f1a65bb328f9c6a9cff146a4
KavinderSingh <51209505+KavinderSingh@users.noreply.github.com>
2021-08-14 22:20:33 +0530

    fixed candle blowing

--- src/components/celebration/index.js
+++ src/components/celebration/index.js
@@ -5,6 +5,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import {
     Container,
     Grid,
+    Button,
     Typography
 } from '@material-ui/core';
 import './style.css';
@@ -16,21 +17,53 @@ const useStyles = makeStyles(styles);
 export default function Celebrate() {
     const classes = useStyles();
     const dispatch = useDispatch();
+    const [mantra, setMantra] = useState()
     const [blowCandle, setBlowCandle] = useState(false)
+    const [note, setNote] = useState("Spell");
+    const [checkSpell, setCheckSpell] = useState();
 
-    const flame = () => blowCandle ? "" : "flame"
 
-    console.log("Asda", flame(), blowCandle)
+    var flame = "flame";
+
     useEffect(() => {
         dispatch(fetchRecordedMantra())
     }, []);
+    useEffect(() => {
+        if (checkSpell === defaultMantra) {
+            flame = "";
+        }
+    }, [checkSpell])
 
-    const mantra = useSelector(state => state.mantra?.mantra);
+    const defaultMantra = useSelector(state => state.mantra?.mantra);
 
     function blowCandles() {
         setBlowCandle(true)
     }
+    const runSpeechRecognition = () => {
+        let SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+        let recognition = new SpeechRecognition();
 
+        // This runs when the speech recognition service starts
+        recognition.onstart = function () {
+            setNote("Spell")
+            //action.innerHTML = "<small>listening, please speak...</small>";
+        };
+
+        recognition.onspeechend = function () {
+            setNote("Spelled")
+            recognition.stop();
+        }
+
+        // This runs when the speech recognition service returns result
+        recognition.onresult = function (event) {
+            let transcript = event.results[0][0].transcript;
+            setMantra(transcript);
+            setCheckSpell(transcript)
+        };
+
+        // start recognition
+        recognition.start();
+    }
     return (
         <Grid component="main" maxWidth="xs" direction='row'>
             <CssBaseline />
@@ -41,14 +74,25 @@ export default function Celebrate() {
                 </Typography>
             </Grid>
             <Grid item className={classes.recordMantra} >
-                <RecordMantra
+                {/* <RecordMantra
                     initialButtonText={"Spell"}
                     displayText={"You Spelled"}
                     onSayButtonText={"Spell"}
                     onStopButtonText={"Spelled"}
                     isCeleb
                     blowCandles={blowCandles}
-                />
+                /> */}
+
+                <Grid className={classes.mantraHeader}>
+                    <Grid item>
+                        <Button variant="contained" color="primary" onClick={runSpeechRecognition}>
+                            {note}
+                        </Button>
+                    </Grid>
+                    <Grid item className={classes.mantraText}>
+                        {(<h4 > "You spelled"- {mantra}</h4>)}
+                    </Grid>
+                </Grid>
             </Grid>
             <Grid item>
                 <div className="cake">
@@ -61,7 +105,7 @@ export default function Celebrate() {
                     <div className="drip drip2"></div>
                     <div className="drip drip3"></div>
                     <div className="candle">
-                        <div className={flame()}> </div>
+                        <div className={flame}> </div>
                     </div>
                 </div>
             </Grid>
